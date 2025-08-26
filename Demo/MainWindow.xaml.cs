b9057c35378170d1d5092fc6e56502d6ab5b8f0f
Tony Hallett <tonyhallett74@gmail.com>
2025-08-26 19:16:06 +0100

    Change to use helper controls for Normal.  Slight adjust to be made to pass tests

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System.Windows;
+﻿using DemoCommon;
 
 namespace Demo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml.
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : DemoWindow
     {
         public MainWindow() => InitializeComponent();
     }
